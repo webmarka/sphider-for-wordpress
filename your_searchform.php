585ffe66952ea8a2ff99328d0c7fd57b324677e5
chasecrawford <chase@ch4ze.com>
2015-03-23 14:06:29 -0400

    deleted unnecessary files

--- your_searchform.php
+++
@@ -1,9 +0,0 @@
-<!--To search through Sphider-->
-<form method="get" id="searchform" action="http://localhost/">
-<label class="hidden" for="query">Search site for:</label>
-<div><input type="text" value="" name="query" id="query" />
-<input type="hidden" name="s" value="1" />
-<input type="submit" id="searchsubmit" value="Search" />
-</div>
-</form>
-
